222e4353e341bdeaccf2998d9f32e1ea4ecfeded
Justin Jia <justin.jia.developer@gmail.com>
2015-11-08 16:20:46 -0800

    Add initital project

---
+++ PGMappingKit/PGMappingKit.h
@@ -0,0 +1,19 @@
+//
+//  PGMappingKit.h
+//  PGMappingKit
+//
+//  Created by Justin Jia on 11/8/15.
+//  Copyright © 2015 Plot Guru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGMappingKit.
+FOUNDATION_EXPORT double PGMappingKitVersionNumber;
+
+//! Project version string for PGMappingKit.
+FOUNDATION_EXPORT const unsigned char PGMappingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGMappingKit/PublicHeader.h>
+
+
