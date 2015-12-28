d661bab75b2008cac7fefe68cc818a146ceaf4e3
hooman96 <hooman.mohammadi1996@gmail.com>
2015-12-27 19:56:33 -0800

    comments added for documentation of PGNetworkHandler

--- PGMappingKit/PGNetworkHandler.h
+++ PGMappingKit/PGNetworkHandler.h
@@ -37,25 +37,57 @@ typedef NS_ENUM(NSInteger, PGSaveOption) {
 
 @interface PGNetworkHandler : NSObject
 
+/**
+ * baseURL is the name for the URL related to the request
+ */
 @property (strong, nonatomic, readonly) NSURL *baseURL;
+
+/**
+ *  determine whether the request is canceled or not
+ */
 @property (nonatomic, getter=isCanceled) BOOL canceled;
 
+/**
+ *  intialize the object with the baseURL
+ */
 - (instancetype)initWithBaseURL:(NSURL *)baseURL NS_DESIGNATED_INITIALIZER;
 
+/**
+ *  instance method dataFromObject maps the object with 'PGNetworkMapping' and stores the data in a dictionary and returns the data.
+ */
 - (NSMutableDictionary *)dataFromObject:(id)object mapping:(PGNetworkMapping *)mapping;
 
+/**
+ *  HTTP Method to create
+ */
 - (void)POST:(NSString *)URLString from:(id)object mapping:(PGNetworkMapping *)mapping success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
 
 - (void)POST:(NSString *)URLString from:(NSDictionary *)data success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
 
+/**
+ *  HTTP Method to update
+ */
 - (void)PUT:(NSString *)URLString from:(id)object mapping:(PGNetworkMapping *)mapping success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
 
 - (void)PUT:(NSString *)URLString from:(NSDictionary *)data success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
 
+/**
+ *  HTTP Method to read for RESTful services
+ */
 - (void)GET:(NSString *)URLString to:(NSManagedObjectContext *)context mapping:(PGNetworkMapping *)mapping option:(PGSaveOption)saveOption success:(void (^)(NSArray *results))success failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
 
 - (void)GET:(NSString *)URLString success:(void (^)(id results))success failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
 
+/**
+ *  HTTP Method to delete for RESTful services
+ * 404 (Not Found), unless you want to delete the whole collection—not often desirable.	
+ 
+ -(void)DELETE:(NSString *)URLString from:(id)object mapping:(PGNetworkMapping *)mapping success:(void (^)(id responseObject))sucess failure:(void (^)(NSError *error))failure finish:(void (^)())finish;
+ */
+
+/**
+ *  This method handles the authorization of the user
+ */
 - (void)setAuthorizationHeaderFieldWithUsername:(NSString *)username password:(NSString *)password;
 - (void)clearAuthorizationHeader;
 
